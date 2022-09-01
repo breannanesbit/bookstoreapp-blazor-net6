5391dc34482125a7f31a18e52e6155e0f2873119
breannanesbit <brebrenesbit02@gmail.com>
2022-09-01 17:51:22 -0600

    added basic database

---
+++ bookstoreapp/bookstoreappApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookstoreappApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("made call to weath endpoint");
+            try
+            {
+                throw new Exception("this is our logging test");
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "fatal Error Occurred");
+                throw;
+            }
+
+        }
+    }
+}
---
+++ bookstoreapp/bookstoreappApi/Data/bookstoredbContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace bookstoreappApi.Data
+{
+    public partial class bookstoredbContext : DbContext
+    {
+        public bookstoredbContext()
+        {
+        }
+
+        public bookstoredbContext(DbContextOptions<bookstoredbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; } = null!;
+        public virtual DbSet<Book> Books { get; set; } = null!;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.Property(e => e.Bio)
+                    .HasMaxLength(250)
+                    .HasColumnName("BIo")
+                    .IsFixedLength();
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsFixedLength();
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsFixedLength();
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasIndex(e => e.Isbn, "UQ__Books__447D36EAEB59D7BA")
+                    .IsUnique();
+
+                entity.Property(e => e.Image)
+                    .HasMaxLength(50)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Isbn)
+                    .HasMaxLength(50)
+                    .HasColumnName("ISBN")
+                    .IsFixedLength();
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+
+                entity.Property(e => e.Summary)
+                    .HasMaxLength(250)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Title).HasMaxLength(50);
+
+                entity.HasOne(d => d.Author)
+                    .WithMany(p => p.Books)
+                    .HasForeignKey(d => d.AuthorId)
+                    .HasConstraintName("FK_Books_ToTable");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
